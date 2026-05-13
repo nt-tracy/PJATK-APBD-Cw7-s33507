4833a8079e715fbb129cd1cb704b5740269e7aaa
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-05-13 23:33:37 +0200

    Implemented IPCsService and built structure of PCsService

---
+++ APBD_C07/Services/IPCsService.cs
@@ -0,0 +1,14 @@
+﻿using APBD_C07.DTOs;
+using APBD_C07.Models;
+
+namespace APBD_C07.Services;
+
+public interface IPCsService
+{
+    Task<IEnumerable<PCsDetails>> GetAllAsync(CancellationToken cancellationToken);
+    Task<ComponentDetailsResponse> GetByIdAsync(int id, CancellationToken cancellationToken);
+    Task<PCsDetails> AddAsync(CreatedPCsRequest request, CancellationToken cancellationToken);
+    Task UpdateAsync(int id, UpdatePCsRequest request, CancellationToken cancellationToken);
+    Task DeleteAsync(int id, CancellationToken cancellationToken);
+
+}
---
+++ APBD_C07/Services/PCsService.cs
@@ -0,0 +1,33 @@
+﻿using APBD_C07.DTOs;
+using APBD_C07.Infrastructure;
+
+namespace APBD_C07.Services;
+
+public class PCsService(DatabaseContext  ctx) : IPCsService
+{
+
+    public async Task<IEnumerable<PCsDetails>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ComponentDetailsResponse> GetByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<PCsDetails> AddAsync(CreatedPCsRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAsync(int id, UpdatePCsRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteAsync(int id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
